cac56a0f7ff5abdad83089d9388e3e4213726bfa
thomasyimgit <thomasyim94@gmail.com>
2017-11-10 14:52:27 -0500

    add google search

--- src/handlers/voice_processing.py
+++ src/handlers/voice_processing.py
@@ -4,11 +4,17 @@ meaningless_words = [
     'please'
 ]
 
-youtube_filter = ['play', 'youtube']
+youtube_meaningless_words = ['play', 'search', 'youtube']
+google_meaningless_words = ['search', 'google']
 
 def filter_words(str):
     return ' '.join(filter(lambda x: x not in meaningless_words, str.lower().split(' ')))
 
+def topic_filter(str, topic):
+    return ' '.join(filter(lambda x: x not in meaningless_words and x not in topic, str.split(' ')))
+
 def filter_youtube(str):
-    str = filter_words(str)
-    return ' '.join(filter(lambda x: x not in youtube_filter, str.split(' ')))
+    return topic_filter(str, youtube_meaningless_words)
+
+def filter_google(str):
+    return topic_filter(str, google_meaningless_words)
